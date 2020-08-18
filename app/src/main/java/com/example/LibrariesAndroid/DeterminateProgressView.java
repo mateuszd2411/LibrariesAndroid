b6b36e2a270b5e97142d627dabf6889bab5cd431
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-18 10:04:56 +0200

    Determinate-Progress-View added animation

--- app/src/main/java/com/example/LibrariesAndroid/DeterminateProgressView.java
+++ app/src/main/java/com/example/LibrariesAndroid/DeterminateProgressView.java
@@ -2,7 +2,9 @@ package com.example.LibrariesAndroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,21 +18,39 @@ public class DeterminateProgressView extends AppCompatActivity {
 
     Button button;
 
+    Handler handler;
+    Runnable runnable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_determinate_progress_view);
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (progress.getProgress() < 100) {
+                    progress.animateProgressChange(progress.getProgress() + 10, 1000);
+                    handler.postDelayed(runnable, 1000);
+                }
+            }
+        };
 
         progress = findViewById(R.id.progress);
         button = findViewById(R.id.button);
 
         progress.setMaxValue(100);
         progress.setProgress(0);
+        progress.setTextEnabled(true);
+        progress.setTextColor(Color.GREEN);
+        progress.setStrokeWidth(10);
+        progress.setStrokeColor(Color.GRAY);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progress.setProgress(progress.getProgress() + 10);
+                handler.postDelayed(runnable, 1000);
             }
         });
     }
