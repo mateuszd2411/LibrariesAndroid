d8be7b0d5707dbb439bf12a9c8d68278f0d00e87
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-18 12:28:10 +0200

    Flip_Digit Clock

--- app/src/main/java/com/example/LibrariesAndroid/FlipDigitClock.java
+++ app/src/main/java/com/example/LibrariesAndroid/FlipDigitClock.java
@@ -7,8 +7,10 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
+import com.aldrek.digitflip.FlipDigit;
 import com.aldrek.digitflip.FlipWidget;
 
+import java.util.Calendar;
 import java.util.Random;
 
 public class FlipDigitClock extends AppCompatActivity {
@@ -16,8 +18,9 @@ public class FlipDigitClock extends AppCompatActivity {
     //https://www.youtube.com/watch?v=_Rv6rCdLnwY&list=PLbqix4Yb1xFLjoMSOBNn65Iqys2lsvZ8w&index=3
 
     Button button;
-    FlipWidget flipWidget;
-    int number = 0;
+    FlipWidget hours, minutes, seconds;
+
+    int hoursNumber, minutesNumber, secondsNumber;
 
     Handler handler;
     Runnable runnable;
@@ -27,27 +30,33 @@ public class FlipDigitClock extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flip_digit_clock);
 
+        hours = findViewById(R.id.hours);
+        minutes = findViewById(R.id.minutes);
+        seconds = findViewById(R.id.seconds);
+
         handler = new Handler();
         runnable = new Runnable() {
             @Override
             public void run() {
-                number++;
-                flipWidget.setValue(number, true);
+
+                Calendar calendar = Calendar.getInstance();
+
+                hoursNumber = calendar.get(Calendar.HOUR_OF_DAY);
+                minutesNumber = calendar.get(Calendar.MINUTE);
+                secondsNumber = calendar.get(Calendar.SECOND);
+
+                hours.setValue(hoursNumber, true);
+                minutes.setValue(minutesNumber, true);
+                seconds.setValue(secondsNumber, true);
+
                 handler.postDelayed(runnable, 1000);
             }
         };
 
-        flipWidget = findViewById(R.id.flipWidget);
-        button = findViewById(R.id.button);
+        handler.postDelayed(runnable, 1000);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (number == 0) {
-                    handler.postDelayed(runnable, 1000);
-                }
-            }
-        });
+
+
 
     }
 }
