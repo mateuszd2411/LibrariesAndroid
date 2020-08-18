994a22ffbcdff6c6390b3f048ec819f5c68d7f1f
mateuszd2411 <mateuszd2411@gmail.com>
2020-08-18 12:02:30 +0200

    Flip_Digit counting down/up

--- app/src/main/java/com/example/LibrariesAndroid/FlipDigitClock.java
+++ app/src/main/java/com/example/LibrariesAndroid/FlipDigitClock.java
@@ -3,6 +3,7 @@ package com.example.LibrariesAndroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,13 +16,26 @@ public class FlipDigitClock extends AppCompatActivity {
     //https://www.youtube.com/watch?v=_Rv6rCdLnwY&list=PLbqix4Yb1xFLjoMSOBNn65Iqys2lsvZ8w&index=3
 
     Button button;
-
     FlipWidget flipWidget;
+    int number = 0;
+
+    Handler handler;
+    Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flip_digit_clock);
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                number++;
+                flipWidget.setValue(number, true);
+                handler.postDelayed(runnable, 1000);
+            }
+        };
 
         flipWidget = findViewById(R.id.flipWidget);
         button = findViewById(R.id.button);
@@ -29,8 +43,9 @@ public class FlipDigitClock extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Random r = new Random();
-                flipWidget.setValue(r.nextInt(10000),true);
+                if (number == 0) {
+                    handler.postDelayed(runnable, 1000);
+                }
             }
         });
 
