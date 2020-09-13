22147e04a65e91a7160fbac1dd7bb45c0199ea1d
mateuszd2411 <mateuszd2411@gmail.com>
2020-09-13 18:25:42 +0200

    Set DropDownView Library

--- app/src/main/java/com/ganarstudio/LibrariesAndroid/DropDownViewTxt.java
+++ app/src/main/java/com/ganarstudio/LibrariesAndroid/DropDownViewTxt.java
@@ -3,6 +3,10 @@ package com.ganarstudio.LibrariesAndroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.anthonyfdev.dropdownview.DropDownView;
 
 public class DropDownViewTxt extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class DropDownViewTxt extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drop_down_view_txt);
+
+        final DropDownView dropDownView = (DropDownView) findViewById(R.id.drop_down_view);
+        View collapsedView = LayoutInflater.from(this).inflate(R.layout.header, null, false);
+        View expandedView = LayoutInflater.from(this).inflate(R.layout.footer, null, false);
+
+        dropDownView.setHeaderView(collapsedView);
+        dropDownView.setExpandedView(expandedView);
+
+        collapsedView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dropDownView.isExpanded()) {
+                    dropDownView.collapseDropDown();
+                } else {
+                    dropDownView.expandDropDown();
+                }
+            }
+        });
     }
 }
