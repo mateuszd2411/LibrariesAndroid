0b8535f269990be3b816213db3ee2cdbbf495be8
mateuszd2411 <mateuszd2411@gmail.com>
2020-09-14 18:44:27 +0200

    Added StyleableToastLibrary https://github.com/Muddz/StyleableToast

--- app/src/main/java/com/ganarstudio/LibrariesAndroid/StyleableToastLibrary.java
+++ app/src/main/java/com/ganarstudio/LibrariesAndroid/StyleableToastLibrary.java
@@ -2,7 +2,12 @@ package com.ganarstudio.LibrariesAndroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.muddzdev.styleabletoast.StyleableToast;
 
 public class StyleableToastLibrary extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class StyleableToastLibrary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_styleable_toast_library);
+
+        Button button = (Button) findViewById(R.id.styleable_toast_btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new StyleableToast
+                        .Builder(StyleableToastLibrary.this)
+                        .text("Hello world!")
+                        .textColor(Color.RED)
+                        .backgroundColor(Color.BLACK)
+                        .stroke(1, Color.WHITE)
+                        .iconStart(R.drawable.food)
+                        .textBold()
+                        .cornerRadius(10)
+                        .length(1000000000)
+                        .show();
+            }
+        });
     }
 }
