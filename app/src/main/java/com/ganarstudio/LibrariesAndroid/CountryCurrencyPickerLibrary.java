4a8431a958e7df0daa90a26e19abdd3bedf92333
mateuszd2411 <mateuszd2411@gmail.com>
2020-09-15 19:24:58 +0200

    CountryCurrencyPickerLibrary, changed jetbrains implementation in gradle

--- app/src/main/java/com/ganarstudio/LibrariesAndroid/CountryCurrencyPickerLibrary.java
+++ app/src/main/java/com/ganarstudio/LibrariesAndroid/CountryCurrencyPickerLibrary.java
@@ -3,6 +3,12 @@ package com.ganarstudio.LibrariesAndroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.scrounger.countrycurrencypicker.library.Buttons.CountryCurrencyButton;
+import com.scrounger.countrycurrencypicker.library.Country;
+import com.scrounger.countrycurrencypicker.library.Currency;
+import com.scrounger.countrycurrencypicker.library.Listener.CountryCurrencyPickerListener;
 
 public class CountryCurrencyPickerLibrary extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class CountryCurrencyPickerLibrary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country_currency_picker_library);
+
+        CountryCurrencyButton button = (CountryCurrencyButton) findViewById(R.id.button);
+        button.setOnClickListener(new CountryCurrencyPickerListener() {
+            @Override
+            public void onSelectCountry(Country country) {
+                if (country.getCurrency() == null) {
+                    Toast.makeText(CountryCurrencyPickerLibrary.this,
+                            String.format("name: %s\ncode: %s", country.getName(), country.getCode())
+                            , Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(CountryCurrencyPickerLibrary.this,
+                            String.format("name: %s\ncurrencySymbol: %s", country.getName(), country.getCurrency().getSymbol())
+                            , Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onSelectCurrency(Currency currency) {
+
+            }
+        });
+
     }
 }
