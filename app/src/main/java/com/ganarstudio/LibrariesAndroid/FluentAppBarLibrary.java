d75743fcf0e81c465b5aff9c13c8d040ee9d483e
mateuszd2411 <mateuszd2411@gmail.com>
2020-10-07 09:54:01 +0200

    Bottom bar https://github.com/byvlstr/fluentAppBar/blob/master/app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/ganarstudio/LibrariesAndroid/FluentAppBarLibrary.java
+++ app/src/main/java/com/ganarstudio/LibrariesAndroid/FluentAppBarLibrary.java
@@ -3,12 +3,46 @@ package com.ganarstudio.LibrariesAndroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class FluentAppBarLibrary extends AppCompatActivity {
+import com.vlstr.fluentappbar.FluentAppBar;
+
+public class FluentAppBarLibrary extends AppCompatActivity implements View.OnClickListener {
+
+    FluentAppBar fluentAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fluent_app_bar_library);
+
+        fluentAppBar = (FluentAppBar) findViewById(R.id.fluent_app_bar);
+        fluentAppBar.setNavigationMenu(R.menu.menu, this);
+        fluentAppBar.setSecondaryMenu(R.menu.menu2, this);
+        fluentAppBar.setBlurRadius(10);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int resId = (int) v.getTag();
+        switch (resId){
+            //Navigation Menu
+            case R.id.nav_all:
+                fluentAppBar.collapse();
+                break;
+            case R.id.nav_album:
+                break;
+            case R.id.nav_keywords:
+                break;
+
+            // Secondary Menu
+            case R.id.menu_sync:
+                fluentAppBar.collapse();
+                break;
+            case R.id.menu_assistant:
+                break;
+            case R.id.menu_shared:
+                break;
+        }
     }
 }
